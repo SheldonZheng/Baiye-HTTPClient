a82dcd139167b215e7820d1e9a9762fdd1afba91
SheldonZheng <zhenghangtxdyr@gmail.com>
2016-08-21 13:20:08 +0800

    实现第一个返回数据类;

---
+++ src/main/java/com/baiye/Model/HTTPResData.java
@@ -0,0 +1,35 @@
+package com.baiye.Model;
+
+/**
+ * Created by Baiye on 8/21/16.
+ */
+public class HTTPResData {
+
+    private String head;
+    private String content;
+    private String contentLength;
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContentLength() {
+        return contentLength;
+    }
+
+    public void setContentLength(String contentLength) {
+        this.contentLength = contentLength;
+    }
+}
