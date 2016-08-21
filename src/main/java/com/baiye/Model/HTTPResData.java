c3f29b2c314ea68f9872a7b4a2fe0c90570ee8ef
SheldonZheng <zhenghangtxdyr@gmail.com>
2016-08-21 14:26:11 +0800

    修改HTTP返回数据体;

--- src/main/java/com/baiye/Model/HTTPResData.java
+++ src/main/java/com/baiye/Model/HTTPResData.java
@@ -5,9 +5,21 @@ package com.baiye.Model;
  */
 public class HTTPResData {
 
+
+    public HTTPResData() {
+    }
+
+    public HTTPResData(String head, String content, int contentLength)
+    {
+        this.head = head;
+        this.content = content;
+        this.contentLength = contentLength;
+    }
+
     private String head;
     private String content;
-    private String contentLength;
+    private int contentLength;
+
 
     public String getHead() {
         return head;
@@ -25,11 +37,11 @@ public class HTTPResData {
         this.content = content;
     }
 
-    public String getContentLength() {
+    public int getContentLength() {
         return contentLength;
     }
 
-    public void setContentLength(String contentLength) {
+    public void setContentLength(int contentLength) {
         this.contentLength = contentLength;
     }
 }
