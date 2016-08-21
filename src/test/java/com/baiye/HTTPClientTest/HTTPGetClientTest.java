520651ed08b436057f393a0e06202d128e14d737
SheldonZheng <zhenghangtxdyr@gmail.com>
2016-08-21 14:28:11 +0800

    加入JUnit单元测试;HTTPGetClient单元测试通过;

---
+++ src/test/java/com/baiye/HTTPClientTest/HTTPGetClientTest.java
@@ -0,0 +1,29 @@
+package com.baiye.HTTPClientTest;
+
+import com.baiye.HTTPClient.HTTPClient;
+import com.baiye.HTTPClient.HTTPGetClient;
+import com.baiye.Model.HTTPResData;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Baiye on 8/21/16.
+ */
+public class HTTPGetClientTest {
+
+    @Test
+    public void sendRequestTest()
+    {
+        HTTPClient client = new HTTPGetClient();
+        HTTPResData resData = null;
+        try {
+            resData = client.sendRequest("http://127.0.0.1:8080/test","");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertNotNull(resData.getHead());
+        Assert.assertNotNull(resData.getContent());
+        Assert.assertNotNull(resData.getContentLength());
+    }
+}
